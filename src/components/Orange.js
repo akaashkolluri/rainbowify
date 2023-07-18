83861aa89fd28d2527aeacdc3edf94868d6cdd46
Akaash Kolluri <arkolluri@gmail.com>
2023-07-17 21:51:51 -0500

    orange

--- src/components/Orange.js
+++ src/components/Orange.js
@@ -2,7 +2,7 @@ import styles from "../style/orange.module.css";
 import React from "react";
 import ReactWordcloud from "react-wordcloud";
 import { Image } from "antd";
-import RedLogo from "../red.png";
+import OrangeLogo from "../orange.png";
 // import "./App.css";
 import "@fontsource/aileron";
 
@@ -23,14 +23,15 @@ function Orange({ urls }) {
         <img className={styles.img11} src={urls[10]} />
         <img className={styles.img12} src={urls[11]} />
         <img className={styles.img13} src={urls[12]} />
-        {/* <img className={styles.Orange} src={RedLogo} /> */}
-        <div className={styles.block1} />
+        <img className={styles.img14} src={urls[13]} />
+        <img className={styles.orange} src={OrangeLogo} />
+        {/* <div className={styles.block1} />
         <div className={styles.block2} />
         <div className={styles.block3} />
         <div className={styles.block4} />
         <div className={styles.block5} />
         <div className={styles.block6} />
-        <div className={styles.block7} />
+        <div className={styles.block7} /> */}
       </div>
     </div>
   );
